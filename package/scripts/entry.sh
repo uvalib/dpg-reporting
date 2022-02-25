fd05c1dcd505155f2db298bb32874d8608b59678
Dave Goldstein <dpg3k@virginia.edu>
2022-02-25 09:12:24 -0500

    Database parameters in the startup

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -4,7 +4,7 @@
 #
 
 # run the server
-cd bin; ./dpg-reporting-svc
+cd bin; ./dpg-reporting-svc -dbhost $DBHOST -dbport $DBPORT -dbname $DBNAME -dbuser $DBUSER -dbpass $DBPASS
 
 # return the status
 exit $?
